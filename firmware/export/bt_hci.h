7db36fa10b391fd6dde3c11ba7dc8c99cc1f531b
Skye <skye@is-quite.gay>
2026-05-18 17:48:53 +0900

    bt: add bt_hci interface
    Change-Id: I8f0d58a409b01eb9d9066c0a7d9764b9310edd8e

---
+++ firmware/export/bt_hci.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2026 Skye Green
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#ifndef BT_HCI_H
+#define BT_HCI_H
+
+// Enables and registers the HCI driver for the target.
+// Can spawn up to one thread for this.
+void bt_hci_enable(void);
+
+// Disables the HCI driver for the target.
+// If bt_hci_enable spawned any threads, they must exit now.
+// Guaranteed to be called after Bluedroid deinit.
+void bt_hci_disable(void);
+
+#endif
