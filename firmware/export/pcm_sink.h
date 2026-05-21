f24f42cc32573d712700ba545a36fbd7a6537b6f
Skye <skye@is-quite.gay>
2026-05-21 16:57:24 +0900

    pcm_sink: add swvol/hwvol metadata
    This is only useful with the next patch; I split this as its own thing because this touches many files in ~identical ways
    
    Change-Id: I5b0a66f2e41ca32a2d6c88bda3ffe0db0df08ce0

--- firmware/export/pcm_sink.h
+++ firmware/export/pcm_sink.h
@@ -23,10 +23,14 @@
 
 #include "config.h"
 
+#define PCM_SINK_HWVOL 0
+#define PCM_SINK_SWVOL 1
+
 struct pcm_sink_caps {
     const unsigned long* samprs;
-    uint16_t             num_samprs;
-    uint16_t             default_freq;
+    uint8_t              num_samprs;
+    uint8_t              default_freq;
+    uint8_t              volume_type;
 };
 
 struct pcm_sink_ops {
